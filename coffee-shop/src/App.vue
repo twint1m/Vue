56e8962e2ff518d88e717c57953a1c2af1703790
twint1m <twint1m.contact@gmail.com>
2023-06-06 17:06:56 +0300

    added vue router

--- coffee-shop/src/App.vue
+++ coffee-shop/src/App.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="wrapper">
-    <hero-view />
+  <div id="wrapper">
+    <router-view/>
+
     <footer-component />
   </div>
 </template>
 
 <script>
-import HeroView from '@/views/HeroView.vue';
-import FooterComponent from '@/components/FooterComponent.vue';
+import HeroView from './views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
+
 export default {
-  components: { HeroView, FooterComponent }
-}
+  components: { HeroView, FooterComponent },
+};
 </script>
 
-<style>
-
+<style lang="scss">
 </style>
